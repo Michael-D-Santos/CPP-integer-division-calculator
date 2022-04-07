6ca300d4898e8d4bb1b9e49127c04540bfd669f3
TSC7 <michaelsantos1337@gmail.com>
2022-04-06 21:15:05 -0400

    C++ integer divison calculator

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+  std::cout << "INTEGER DIVISION CALCULATOR:\n\n";
+  
+  std::string Dividend;
+  std::string Divisor;
+  
+  std::cout << "What do you want to be the dividend? ";
+  //Prompts user for Dividend
+  std::cin >> Dividend;
+  std::cout << "What do you want to be the divisor? ";
+  //Prompts user for Divisor
+  std::cin >> Divisor;
+  
+  //If either the dividend or divisor are decimals, append "0" to the
+  //beginning of the string to properly convert the strings to decimal.
+  
+  if (Dividend[0] == '.') {
+      Dividend = Dividend.insert(0, "0");
+  }
+  if (Divisor[0] == '.') {
+      Divisor = Divisor.insert(0, "0");
+  }
+  
+  /*
+  Calculates the quotient obtained by integer division and the remainder
+  If and else-if statement preform excpetion handling (i.e. if either the 
+  divisor or the divisor AND the dividend are 0, return "undefined")
+  */
+  
+  
+  if (round(std::stoi(Divisor)) == 0) {
+      std::cout << "ERROR: " << round(std::stoi(Dividend)) << "/" << "0" << 
+      " is undefined";
+  } else if (round(std::stoi(Dividend)) && round(std::stoi(Divisor)) == 0) {
+      std::cout << "ERROR: " << "0" << "/" << "0" << " is undefined";
+  } else {
+  std::cout << Dividend << "/" << Divisor  << " = " << 
+  (std::stoi(Dividend)/std::stoi(Divisor)) << " Remainder " << 
+  (std::stoi(Dividend)%std::stoi(Divisor));
+  }
+}
